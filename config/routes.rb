bea6e943619b9ea34d5ec5d1a30ec8da6109b223
Jake Loose <jetloose@gmail.com>
2017-01-09 11:30:21 +0000

    Add account Activation

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,33 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
+  resources :account_activations , only: [:edit]
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
