cfd3dc67e5bff2b6ea11fbdb1f48c5c87dd53c52
Jake Loose <jetloose@gmail.com>
2017-01-25 07:33:56 +0000

    Add feed modifications to show posts from followed users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -88,4 +88,22 @@ class UserTest < ActiveSupport::TestCase
     jake.unfollow(archer)
     assert_not jake.following?(archer)
   end
-end
+  
+  test "feed should have the right posts" do
+    jake = users(:jake)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert jake.feed.include?(post_following)
+    end
+    # Posts from self
+    jake.microposts.each do |post_self|
+      assert jake.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not jake.feed.include?(post_unfollowed)
+    end
+  end
+end 
